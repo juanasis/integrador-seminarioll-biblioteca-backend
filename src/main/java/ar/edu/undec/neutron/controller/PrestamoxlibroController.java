259ea9736305c09983dd9ec170d4d77019632a63
juan asis <juan.asis1234@gmail.com>
2020-11-18 17:51:18 -0300

    se modifican controladores y servicios

--- src/main/java/ar/edu/undec/neutron/controller/PrestamoxlibroController.java
+++ src/main/java/ar/edu/undec/neutron/controller/PrestamoxlibroController.java
@@ -1,7 +1,10 @@
 package ar.edu.undec.neutron.controller;
 
 import ar.edu.undec.neutron.dto.Response;
+import ar.edu.undec.neutron.modelo.Persona;
 import ar.edu.undec.neutron.modelo.Prestamo;
+import ar.edu.undec.neutron.modelo.Prestamoxlibro;
+import ar.edu.undec.neutron.service.PersonaService;
 import ar.edu.undec.neutron.service.PrestamoService;
 import ar.edu.undec.neutron.service.PrestamoxlibroService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,22 +12,32 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+
 @CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/prestamoxlibro")
 public class PrestamoxlibroController {
     @Autowired
     PrestamoxlibroService prestamoxlibroService;
+    PersonaService personaService;
+    PrestamoService prestamoService;
 
 
     @GetMapping("/{id}")
-    public ResponseEntity<Response> getPrestamosxlibro(@PathVariable(value = "id")Integer prestamoxlibroid)throws  Exception{
+    public ResponseEntity<Response> getPrestamosxlibro(@PathVariable(value = "id")Integer prestamoxlibroid
+    )throws  Exception{
         Response response = prestamoxlibroService.findOne(prestamoxlibroid);
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
     @GetMapping()
     public ResponseEntity<Response> getTodosLosPrestamos(){
         Response response = prestamoxlibroService.findAll();
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+    @PostMapping("/realizarprestamoxlibro")
+    public ResponseEntity<Response> save(@Valid @RequestBody Prestamoxlibro prestamo){
+        Response response = prestamoxlibroService.save(prestamo);
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
   
--- src/main/java/ar/edu/undec/neutron/service/PrestamoxlibroService.java
+++ src/main/java/ar/edu/undec/neutron/service/PrestamoxlibroService.java
@@ -1,27 +1,18 @@
 package ar.edu.undec.neutron.service;
 
-import ar.edu.undec.neutron.dto.PrestamoxLibroDTO;
 import ar.edu.undec.neutron.dto.Response;
-import ar.edu.undec.neutron.modelo.Prestamo;
 import ar.edu.undec.neutron.modelo.Prestamoxlibro;
-import ar.edu.undec.neutron.repository.LibroRepository;
-import ar.edu.undec.neutron.repository.PersonaRepository;
-import ar.edu.undec.neutron.repository.PrestamoRepository;
 import ar.edu.undec.neutron.repository.PrestamoxlibroRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class PrestamoxlibroService {
     @Autowired
     PrestamoxlibroRepository prestamoxlibroRepository;
-    PrestamoRepository prestamoRepository;
-    PersonaRepository personaRepository;
-    LibroRepository libroRepository;
+
 
     public Response findAll(){
         Response response = new Response();
@@ -30,24 +21,6 @@ public class PrestamoxlibroService {
         return response;
     }
 
-   /*public Response findAllLibrosYPersonas(){
-        Response response = new Response();
-        PrestamoxLibroDTO prestamoxlibrosypersonas = new PrestamoxLibroDTO();
-        ArrayList<PrestamoxLibroDTO> prestamos = new ArrayList<PrestamoxLibroDTO>() {};
-        List<Prestamo> prestamosrepo = new ArrayList<>();
-        prestamosrepo =  prestamoRepository.findAll();
-        for ( Prestamo prestamo: prestamosrepo ) {
-              prestamoxlibrosypersonas.setId(prestamo.getId());
-              prestamoxlibrosypersonas.setNombre(personaRepository.findById(prestamo.getPersonaid()).get().getNombre());
-              prestamoxlibrosypersonas.setApellido(personaRepository.findById(prestamo.getPersonaid()).get().getApellido());
-              prestamoxlibrosypersonas.setTitulo(libroRepository.findById(prestamoxlibroRepository.findById(prestamo.getId()).get().getLibroid()).get().getTitulo());
-              prestamos.add(prestamoxlibrosypersonas);
-        }
-        response.setData(prestamos);
-        return response;
-    }
-
-*/
 
     public Response findOne(Integer libroid){
         Response response = new Response();
